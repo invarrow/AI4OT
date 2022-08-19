02028ec6821a5dfea096304e41e29bcb223d33cd
invarrow <arm25412@gmail.com>
2022-08-19 14:45:13 +0530

    aaa aaa aaaaaa romance

--- fladmin/server.py
+++ fladmin/server.py
@@ -2,23 +2,22 @@
 from flask import Flask, jsonify, render_template, request
 import webbrowser
 import time,csv
-
-file = open('C:\\Users\\rashe\\OneDrive\\Desktop\\otHack\\AI4OT\\test.csv','r')
-csvreader = csv.reader(file)
-
 app = Flask(__name__)
+bigrow = []
+f= open(r"C:\Users\rashe\OneDrive\Desktop\otHack\AI4OT\dataset.csv")
+csvreader = csv.reader(f)
 
 @app.route('/_stuff', methods = ['GET'])
 def stuff():
-    for row in csvreader:
-        print(row)
-        return jsonify(str(row))
-    
-    
+        
+        for row in csvreader:
+            bigrow.append(row)
+            return jsonify(result=str(bigrow))
 
 
 @app.route('/')
 def index():
+   
     return render_template('dy1.html')
 
     
