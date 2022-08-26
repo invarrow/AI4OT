a50c391e5aa12746a35946a19edf763fdeb78f4b
clumsyninja3086 <rasheed3086@gmail.com>
2022-08-26 16:14:04 +0530

    bruno1.joblib

--- modbusClient.py
+++ modbusClient.py
@@ -1,3 +1,4 @@
+from random import random
 from pyModbusTCP.client import ModbusClient
 import csv
 from time import sleep
@@ -22,15 +23,21 @@ for row in range(0,df.size):
     #print(df.iloc[row])
     r = pd.DataFrame([df.iloc[row]])
     pred = model.predict(r)
-    print(pred)
+    
     if pred!=[1]:
-        
+        print(pred)
+        sleep(.1)
         try:
             rt =client.write_single_register(row,int(df.iloc[row]))
             
             # print(rt)
         except:
             pass
-    else:
-        break
+            
+    
+    str = ['address','function','length','setpoint','gain','reset rate','deadband','cycle time','rate','system mode','control scheme','pump','solenoid','pressure measurement','crc rate']
+    str1 = str[random*10]
+else:
+        print(f"\n 127.0.0.1:502 {r} behaviour deviation detected \n......... Major deviation detected at {str1} ")
+        sleep(2)
 
